4aed6287d653396481ff6e0893a76bd62657d20c
Toru Takata <tettou771@gmail.com>
2022-02-06 01:30:23 +0900

    add Serial mode

--- src/ofxBarcodeReader.cpp
+++ src/ofxBarcodeReader.cpp
@@ -2,38 +2,85 @@
 
 
 
-ofxBarcodeReader::ofxBarcodeReader() {
-	ofAddListener(ofEvents().keyPressed, this, &ofxBarcodeReader::keyPressed);
-
+ofxBarcodeReader::ofxBarcodeReader(ofxBarcodeReaderType _type, string _comPort, int _baudRate):
+type(_type), comPort(_comPort) , baudRate(_baudRate){
+    switch(type) {
+        case HIDKeyboardDevice:
+            ofAddListener(ofEvents().keyPressed, this, &ofxBarcodeReader::keyPressed);
+            break;
+        
+        case SerialDevice:
+            // if no port, auto select
+            if (comPort == "") {
+                for (auto d : serial.getDeviceList()) {
+                    comPort = d.getDevicePath();
+                    break;
+                }
+            }
+            
+            serial.setup(comPort, baudRate);
+            
+            ofAddListener(ofEvents().update, this, &ofxBarcodeReader::update);
+            break;
+        default:
+            break;
+    }
 	initialized = true;
-	pastKeyPressedFrameNum = 0;
+	pastKeyPressedSec = pastSerialReceiveSec = 0;
 }
 
 
 ofxBarcodeReader::~ofxBarcodeReader() {
 	if (initialized) {
-		ofRemoveListener(ofEvents().keyPressed, this, &ofxBarcodeReader::keyPressed);
+        switch(type) {
+            case HIDKeyboardDevice:
+                ofRemoveListener(ofEvents().keyPressed, this, &ofxBarcodeReader::keyPressed);
+                break;
+            case SerialDevice:
+                serial.close();
+                break;
+            default:
+                break;
+        }
 	}
 }
 
+void ofxBarcodeReader::addCharactor(char c) {
+    if (c == OF_KEY_RETURN) {
+        if (data != "") {
+            barcodeReaded();
+            data = "";
+        }
+    }
+    
+    // ascii charactor
+    else if (33 <= c && c <= 126) {
+        data += c;
+    }
+}
+
 void ofxBarcodeReader::keyPressed(ofKeyEventArgs & key) {
-	long int now = ofGetFrameNum();
-	int timeoutFrameNum = 2;
-	if (now - pastKeyPressedFrameNum > timeoutFrameNum) {
+	float now = ofGetElapsedTimef();
+	float timeoutSec = 0.2;
+	if (now - pastKeyPressedSec > timeoutSec) {
 		data = "";
 	}
+    
+    addCharactor(key.key);
+    pastKeyPressedSec = now;
+}
 
-	if (key.key == OF_KEY_RETURN) {
-		if (data != "") {
-			barcodeReaded();
-			data = "";
-		}
-	}
-	else {
-		data += key.key;
-	}
-
-	pastKeyPressedFrameNum = now;
+void ofxBarcodeReader::update(ofEventArgs &args) {
+    float now = ofGetElapsedTimef();
+    
+    while (serial.available()) {
+        addCharactor(serial.readByte());
+        pastSerialReceiveSec = now;
+    }
+    
+    if (data != "" && now - pastSerialReceiveSec > 0.1) {
+        addCharactor(OF_KEY_RETURN);
+    }
 }
 
 void ofxBarcodeReader::barcodeReaded() {
--- src/ofxBarcodeReader.h
+++ src/ofxBarcodeReader.h
@@ -5,17 +5,33 @@ struct ofxBarcodeEventArgs {
 	string data;
 };
 
+enum ofxBarcodeReaderType {
+    HIDKeyboardDevice,
+    SerialDevice
+};
+
 class ofxBarcodeReader {
 public:
-	ofxBarcodeReader();
+	ofxBarcodeReader(ofxBarcodeReaderType _type = HIDKeyboardDevice, string _comPort = "", int _baudRate = 115200);
 	~ofxBarcodeReader();
 
 	ofEvent<ofxBarcodeEventArgs> barcodeEvents;
 private:
 	bool initialized;
-	long int pastKeyPressedFrameNum;
+    ofxBarcodeReaderType type;
 	string data;
+    void addCharactor(char c);
+    
+    // HID keyboard device mode
 	void keyPressed(ofKeyEventArgs& key);
 	void barcodeReaded();
+    float pastKeyPressedSec;
+
+    // serial mode
+    void update(ofEventArgs &args);
+    string comPort; // use only SerialDevice mode
+    ofSerial serial;
+    float pastSerialReceiveSec;
+    int baudRate;
 };
 
